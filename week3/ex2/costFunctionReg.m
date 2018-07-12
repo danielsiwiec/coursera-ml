6eff33f53e0a26b2251864d659d997b9b7a73681
Dan Siwiec <daniel.siwiec@gmail.com>
2018-07-12 16:55:24 -0700

    Add regularized cost and gradient implementation

--- week3/ex2/costFunctionReg.m
+++ week3/ex2/costFunctionReg.m
@@ -18,8 +18,9 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
-
-
+J =  1/m * (-y'*log(sigmoid(X*theta)) - (1-y')*log(1-sigmoid(X*theta))) + lambda/(2*m) * (theta(2:end,:)' * theta(2:end,:));
+grad(1,:) = 1/m * ((sigmoid(X*theta)-y)'*X(:,1));
+grad(2:end,:) = 1/m * ((sigmoid(X*theta)-y)'*X(:,2:end)) + lambda/m * theta(2:end)';
 
 
 % =============================================================
