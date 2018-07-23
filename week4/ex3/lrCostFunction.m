2e96407346b30145d4dcc5e200a1b8f78f7fa1a8
Dan Siwiec <daniel.siwiec@gmail.com>
2018-07-22 19:57:18 -0700

    Add one vs all training

--- week4/ex3/lrCostFunction.m
+++ week4/ex3/lrCostFunction.m
@@ -39,7 +39,9 @@ grad = zeros(size(theta));
 
 
 
-
+J =  1/m * (-y'*log(sigmoid(X*theta)) - (1-y')*log(1-sigmoid(X*theta))) + lambda/(2*m) * (theta(2:end,:)' * theta(2:end,:));
+grad(1,:) = 1/m * ((sigmoid(X*theta)-y)'*X(:,1));
+grad(2:end,:) = 1/m * ((sigmoid(X*theta)-y)'*X(:,2:end)) + lambda/m * theta(2:end)';
 
 
 
